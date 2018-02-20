e408943c6ea447cb2d1adf0ba4236d68ef2348f5
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-02-20 21:54:56 +0000

    Now displays new messages onKeyPress 'enter' with random keys so children don't get squashed

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,48 @@ import NavBar from './NavBar.jsx';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
+function generateRandomString() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < 6; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
 class App extends Component {
 // Set initial state so the user is initially anonymous
   constructor(props) {
     super(props);
+    this.brandNewMessage = this.brandNewMessage.bind(this);
     this.state = {
-      currentUser: {name: 'Bob'},
+      currentUser: {name: 'Anonymous'},
       messages: [
-        {currentUser: 'Bob', content: 'Has anyone seen my marbles?', key: 1},
-        {currentUser: 'Anonymous', content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.', key: 2}
+        {currentUser: 'Bob', content: 'Has anyone seen my marbles?', key: generateRandomString()},
+        {currentUser: 'Anonymous', content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.', key: generateRandomString()}
       ]
     };
   }
 
-  // Called after the component was rendered and it was attached to the
-  // DOM. This is a good place to make AJAX requests or setTimeout.
+  brandNewMessage(message) {
+    const newMessage = {currentUser: message.currentUser, content: message.input, key: generateRandomString()};
+    const messages = this.state.messages.concat(newMessage);
+    this.setState({messages: messages});
+  }
+
+  // Called after the component was rendered and it was attached to the DOM.
 componentDidMount() {
   console.log("componentDidMount <App />");
-  setTimeout(() => {
-    console.log("Simulating incoming message");
-    // Add a new message to the list of messages in the data store
-    const newMessage = {currentUser: 'Michelle', content: 'Hello there!', key: 3};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages});
-  }, 3000);
+
 }
 
-  // Called any time the props or state changes. The JSX elements
-  // returned in this method will be rendered to the DOM.
+  // Called any time the props or state changes. The JSX elements returned in this method will be rendered to the DOM.
   render() {
     return (
       <div>
       <NavBar />
       <MessageList messages = {this.state.messages} />
-      <ChatBar defaultValue={this.state.currentUser.name} />
+      <ChatBar defaultValue={this.state.currentUser.name} brandNewMessage={this.brandNewMessage}/>
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
 
 export default class ChatBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {input: '', currentUser: props.defaultValue};
+  }
+
   render() {
+    const changedText = (evt) => {
+      this.setState({input: evt.target.value, currentUser: this.state.currentUser});
+    };
+
+    const changedUser = (evt) => {
+      this.setState({inputValue: this.state.inputValue, currentUser: evt.target.value});
+    };
+
+    const sendMessage = (evt) => {
+      if(evt.key === 'Enter') {
+        this.props.brandNewMessage(this.state);
+        this.setState({input: '', currentUser: this.state.currentUser});
+      }
+    };
+
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" placeholder="Your Name (Optional)" onChange={changedUser} value={this.state.currentUser} />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" onChange={changedText} value={this.state.input} onKeyPress={sendMessage}/>
       </footer>
     )
   }
